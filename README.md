998394b013df42dc3c364ac605aaaa54442d67b8
Dave Blaschke <deblaschke@yahoo.com>
2025-11-26 15:14:39 -0600

    Initial holiday slideshow commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# holiday-slideshow
-Photo slideshow of our holiday travels in the Texas Hill Country 
+
+ASTRONOMY & SPACE PICTURE SLIDESHOW README
+------------------------------------------
+
+To view the slideshow, go to https://deblaschke.github.io/holiday-slideshow/
+in your favorite browser.  When the slideshow is complete (there are two standard
+closing credit slides at the end) or you've had too much, close the tab/window.
+
+Automatic slideshows have three control buttons below the pictures: previous
+slide, pause/play, and next slide.  Manual slideshows only have the previous
+slide and next slide buttons.  The left arrow (previous slide), right arrow
+(next slide), and escape (pause/play) keys can be used in lieu of the control
+buttons.
+
+The slideshows have been tested on the following browsers:
+- Firefox on Windows and macOS
+- Chrome on Windows, macOS and Android (NOTE: Not perfectly responsive on mobile devices)
+- Internet Explorer on Windows (NOTE: Might need to allow blocked content)
+- Safari on macOS
+- Edge on Windows
+
+The following slideshow attibutes can be customized:
+
+   1) Manual/Automatic
+
+      In js/slideshow.js, change MANUAL_SLIDESHOW (default false) to desired
+      value
+
+      NOTE: This can be overridden on the URL through use of the mode query
+      parameter, for example "?mode=manual"
+
+   2) Interval
+
+      In js/slideshow.js, change SLIDESHOW_INTERVAL (default 3000) to desired
+      value in milliseconds
+
+      NOTE: Only applicable if MANUAL_SLIDESHOW is false
+
+      NOTE: This can be overridden on the URL through use of the interval query
+      parameter, for example "?interval=1000"
+
---
+++ js/slideshow.js
@@ -0,0 +1,318 @@
+// Indicates manual (true) or automatic (false) slideshow
+var MANUAL_SLIDESHOW = false;
+// Automatic slideshow interval in milliseconds
+var SLIDESHOW_INTERVAL = 3000;
+// Indicates audio (true) or no audio (false) during slideshow
+var SLIDESHOW_AUDIO = true;
+
+// Current slide index
+var slideIndex;
+// Timeout object (Number representing timer ID)
+var slideshowTimeout = null;
+// Sound object (HTMLAudioElement object)
+var slideshowSound = null;
+// Array of slides with class "holidayPix"
+var slideshowElems = document.getElementsByClassName("holidayPix");
+// Indicates if mobile device (if not detected, default behavior occurs which is acceptable)
+var isMobileDevice = false;
+
+// Allow for override of default behavior in URL via query parameters
+if ("URLSearchParams" in window) {
+  var urlParams = new URLSearchParams(window.location.search);
+  var urlParam = urlParams.get('mode');
+  if (urlParam === 'manual') {
+    MANUAL_SLIDESHOW = true;
+  }
+  urlParam = urlParams.get('interval');
+  if (/^\d+$/.test(urlParam)) {
+    SLIDESHOW_INTERVAL = parseInt(urlParam, 10);
+  }
+  urlParam = urlParams.get('audio');
+  if (urlParam === 'off') {
+    SLIDESHOW_AUDIO = false;
+  }
+}
+
+// getDescription returns description for regular slide
+function getDescription(path) {
+  var result = "";
+
+  // path is of format "*/file.jpg"
+  // file is of format "yymmdd_picture-description" where yy is always two-digit year,
+  //   mm is always two-digit month, and dd is always two-digit day
+
+  // Process valid paths (must have directory separator and .jpg/.gif extension)
+  path = decodeURI(path);
+  var index = path.lastIndexOf('/');
+  if (index >= 0 && path.lastIndexOf('.jpg') > index) {
+    var file = path.substring(index + 1, path.lastIndexOf('.'));
+
+    // File begins with "yymmdd_" or "yymmddT" for slides
+    if (/^[0-9]{6}[_T]{1}/.test(file)) {
+      var date = file.substring(0, 6);
+
+      if (/^[0-9]{6}_[C-Z]{1}[0-9]{7}/.test(file)) {
+        // Found digital camera picture name ("yymmdd_Annnnnnn")
+        result = file.substring(7, 15);
+        index = 15;
+      } else if (/^[0-9]{6}_[A-B]{1}[0-9]{3}-[0-9]{2}/.test(file)) {
+        // Found film camera picture name ("yymmdd_Annn-nn") NOT YET USED!
+        result = file.substring(7, 14);
+        index = 14;
+      } else if (/^[0-9]{6}T[0-9]{6}/.test(file)) {
+        // Found cell phone camera picture name ("yymmddTnnnnnn") NOT YET USED!
+        result = file.substring(0, 6) + file.substring(7, 13);
+        index = 13;
+      }
+
+      // Continue processing if recognized picture name
+      if (result.length > 0) {
+        // Handle description if present
+        index = file.indexOf('-', index);
+        if (index >= 0) {
+          // Picture description is everything after "-"
+          var desc = file.substring(index + 1);
+
+          // Replace underscores with spaces
+          result = result + " - " + desc.replace(/_/g, ' ');
+
+          // Replace special characters ("[*]") with HTML entity names ("&*;")
+          index = file.indexOf('[');
+          if (index >= 0 && index < file.indexOf(']')) {
+            result = result.replace(/\[/g, '&');
+            result = result.replace(/\]/g, ';');
+          }
+        }
+
+        // Get English date from yymmdd
+        var dateStr = "";
+        var month = [ "January", "February", "March", "April",
+                      "May", "June", "July", "August", "September",
+                      "October", "November", "December" ][+date.substring(2, 4) - 1];
+        dateStr = month + " " + (date.charAt(4) == '0' ? date.charAt(5) : date.substring(4, 6)) + ", 20" + date.substring(0, 2);
+        if (dateStr.length > 0) {
+          result = result + "<BR>(" + dateStr + ")";
+        }
+      }
+    }
+  }
+
+  // Display space to occupy slideName span if description empty
+  if (result.length == 0) {
+    result = "&nbsp;<BR>&nbsp;";
+  }
+
+  return result;
+}
+
+// hidePlayButton hides play/pause button for manual slideshows
+function hidePlayButton() {
+  document.getElementById("buttonPlayPause").style.display = "none";
+}
+
+// toggleFlow plays/pauses slideshow as result of user action (mouseclick or keystroke)
+// where elem is play/pause button
+function toggleFlow(elem) {
+  if (slideshowTimeout != null) {
+    // Pause slideshow
+    clearInterval(slideshowTimeout);
+    slideshowTimeout = null;
+
+    // Set button text to ">" (play)
+    elem.innerHTML = "&#9658;";
+    elem.title = "Play";
+
+    // Pause audio if it exists
+    if (slideshowSound != null) {
+      slideshowSound.pause();
+    }
+  } else {
+    // Play slideshow
+    slideshow();
+
+    // Set button text to "||" (pause)
+    elem.innerHTML = "&#10074;&#10074;";
+    elem.title = "Pause";
+
+    // Play audio if it exists
+    if (slideshowSound != null) {
+      slideshowSound.play();
+    }
+  }
+}
+
+// changePic changes slide as result of user action (mouseclick or keystroke)
+// where n is delta (+1 or -1)
+function changePic(n) {
+  showPic(n);
+
+  if (!MANUAL_SLIDESHOW) {
+    // Automatic slideshow
+
+    if (slideshowTimeout != null) {
+      // Set new timeout for new slide
+      clearInterval(slideshowTimeout);
+      slideshowTimeout = setInterval(slideshow, SLIDESHOW_INTERVAL);
+
+      // Play audio if it exists
+      if (slideshowSound != null) {
+        slideshowSound.play();
+      }
+    }
+  } else {
+    // Manual slideshow
+
+    // Play audio if it exists
+    if (slideshowSound != null) {
+      slideshowSound.play();
+    }
+  }
+}
+
+// showPic displays slide where n is change to slideIndex
+function showPic(n) {
+  slideIndex += n;
+
+  // Handle wrapping past end of slideshow
+  if (slideIndex > slideshowElems.length) {slideIndex = 1}
+
+  // Handle wrapping before beginning of slideshow
+  if (slideIndex < 1) {slideIndex = slideshowElems.length}
+
+  // Set all slides to hidden
+  for (var i = 0; i < slideshowElems.length; i++) {
+    slideshowElems[i].style.display = "none";
+  }
+
+  // Set current slide to visible
+  slideshowElems[slideIndex-1].style.display = "block";
+
+  // Set slide description
+  document.getElementById("slideName").innerHTML = getDescription(slideshowElems[slideIndex-1].src);
+}
+
+// slideshow runs automatic slideshow
+function slideshow() {
+  showPic(1);
+
+  // Play slideshow if paused
+  if (slideshowTimeout == null) {
+    slideshowTimeout = setInterval(slideshow, SLIDESHOW_INTERVAL);
+  }
+}
+
+// Handle left arrow, right arrow and pause keys
+document.onkeydown = function(event) {
+  switch (event.key) {
+    case 'ArrowLeft':
+      changePic(-1);
+      break;
+    case 'ArrowRight':
+      changePic(1);
+      break;
+    case 'Escape':
+      if (!MANUAL_SLIDESHOW) {
+        toggleFlow(document.getElementById("buttonPlayPause"));
+      }
+      break;
+  }
+}
+
+// Load and play audio if configured
+if (SLIDESHOW_AUDIO) {
+  // Create audio object
+  slideshowSound = new Audio("media/audio.mp3");
+
+  // Set audio object to loop
+  if (typeof slideshowSound.loop == 'boolean') {
+    slideshowSound.loop = true;
+  } else {
+    slideshowSound.addEventListener('ended', function() {
+      this.currentTime = 0;
+      this.play();
+    }, false);
+  }
+
+  // Play audio object, catching/ignoring any errors
+  promise = slideshowSound.play();
+  if (promise != null) {
+    promise.catch(function(error) { });
+  }
+}
+
+// setPicDimensions sets dimensions of #innerTable based on window dimensions and device type
+function setPicDimensions() {
+  // Find smallest window dimension
+  var minDim = Math.min(window.innerHeight, window.innerWidth);
+
+  // Calculate smallest dimension based on smallest window dimension and device type
+  if (minDim > 842 && isMobileDevice) {
+    minDim = 842; /* 95% of 842 is 800 (bump up mimimum on mobile because width is large but screen is small) */
+  } else if (minDim > 674) {
+    minDim = 674; /* 95% of 674 is 640, which is actual slide resolution */
+  } else if (minDim < 269) {
+    minDim = 269; /* 95% of 269 is 256, which is as small as we want to go */
+  }
+
+  // Set innerTable dimensions (a square) to smallest dimension
+  document.getElementById("innerTable").style.width = minDim + 'px';
+  document.getElementById("innerTable").style.height = minDim + 'px';
+
+  // Set slideName width to smaller than innerTable width
+  document.getElementById("slideName").style.width = (minDim-2) + 'px';
+}
+
+// Handle window load
+window.onload = function() {
+  if (!SLIDESHOW_AUDIO) {
+    // Remove music credit (last slide) if it exists
+    var audioCredit = slideshowElems[slideshowElems.length-1].src;
+    if (audioCredit.indexOf("theend3") != -1) {
+      slideshowElems[slideshowElems.length-1].remove();
+    }
+  }
+}
+
+// Handle window resize
+window.onresize = function() {
+  setPicDimensions();
+}
+
+// Handle DOM loaded event
+document.addEventListener("DOMContentLoaded", (event) => {
+  isMobileDevice = /iPhone|Android|BlackBerry/i.test(navigator.userAgent);
+  setPicDimensions();
+  slideIndex = 0;
+
+  // Initiate slideshow
+  if (MANUAL_SLIDESHOW) {
+    hidePlayButton();
+    showPic(1);
+  } else {
+    slideshow();
+  }
+});
+
+// BEGIN FOR PERSONAL USE ONLY
+// disableScreenTimeout attempts to disable screen timeout when slideshow has focus
+async function disableScreenTimeout() {
+  try {
+    await navigator.wakeLock.request("screen");
+    // Screen timeout disabled
+  } catch (err) {
+    // Screen timeout not disabled
+  }
+}
+
+if ("wakeLock" in navigator) {
+  // Disable screen timeout for initial visibility
+  disableScreenTimeout();
+
+  // Add listener to disable screen timeout for subsequent visibilities
+  document.addEventListener("visibilitychange", async () => {
+    if (document.visibilityState === "visible") {
+      disableScreenTimeout();
+    }
+  });
+}
+// END FOR PERSONAL USE ONLY
